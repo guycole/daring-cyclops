a4be619fbb81aaf21b137ce15e9aef8617cbab88
Guy Cole <guycole@gmail.com>
2021-08-08 23:08:46 -0700

    switch to go

---
+++ infra/minikube/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Title:start.sh
+# Description: start minikube
+# Development Environment: OS X 10.13.6
+# Author: G.S. Cole (guycole at gmail dot com)
+#
+minikube start --memory=6g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0 --vm=true
+#
