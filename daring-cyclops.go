bdf262559b41266a23dd9a4d7f5e0feb1758068e
Guy Cole <guycole@gmail.com>
2021-08-09 17:42:26 -0700

    more experiment

--- daring-cyclops.go
+++ daring-cyclops.go
@@ -8,7 +8,14 @@ func main() {
   mux := http.NewServMux()
   files := http.FileServer(http.Dir("/public"))
   mux.Handle("/static/", http.StripPrefix("/static/"), files)
+
   mux.HandleFunc("/", index)
+  mux.HandleFunc("/err", index)
+
+  mux.HandleFunc("/authenticate", authenticate)
+
+  mux.HandleFunc("/login", login)
+  mux.HandleFunc("/logout", logout)
 
   server := &http.Server {
     Addr:"0.0.0.0:8080", 
