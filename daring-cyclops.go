58ff8e68b1bb14a0a64623d17decca03277b6de3
Guy Cole <gcole@rivian.com>
2021-08-11 16:13:33 -0700

    work in progress

---
+++ daring-cyclops.go
@@ -0,0 +1,12 @@
+// go mod init github.com/guycole/daring-cyclops
+// go install github.com/guycole/daring-cyclops
+
+package main
+
+import (
+	"github.com/guycole/daring-cyclops/manager"
+)
+
+func main() {
+	manager.Manager()
+}
--- manager/manager.go
+++ manager/manager.go
@@ -1,7 +1,47 @@
-package main
+package manager
 
-import "fmt"
+import (
+	"log"
 
-func main() {
-    fmt.Println("hello world")
+	"github.com/google/uuid"
+)
+
+const Banner = "Daring Cyclops V0.0"
+
+const MaxGames = 5
+
+type Game struct {
+	Active       bool
+	RandomId     string
+	SequentialId int
+}
+
+var allGames [MaxGames]Game
+
+var maxSequentialId = -1
+
+func getSequentialId() int {
+	var currentId = maxSequentialId + 1
+	maxSequentialId = currentId
+	return maxSequentialId
+}
+
+func setupGame(ndx int) {
+	log.Println("setupGame:", ndx)
+	allGames[ndx].Active = false
+	allGames[ndx].RandomId = uuid.NewString()
+	allGames[ndx].SequentialId = getSequentialId()
+}
+
+func setup() {
+	for ndx := 0; ndx < MaxGames; ndx++ {
+		setupGame(ndx)
+	}
+}
+
+func Manager() {
+	log.Println(Banner)
+	setup()
+
+	// fall into event loop
 }
