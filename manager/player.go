04ce9294ac7fdae87e04a7e3a8dd56b64c680ee5
Guy Cole <guycole@gmail.com>
2021-09-17 16:20:06 +0000

    redis for manager

---
+++ manager/player.go
@@ -0,0 +1,265 @@
+// Copyright 2021 Guy Cole. All rights reserved.
+// Use of this source code is governed by a GPL-3 license that can be found
+// in the LICENSE file.
+package main
+
+import (
+	"errors"
+	"log"
+	"strings"
+)
+
+// rankEnum
+type rankEnum int
+
+// must match order for legalRanks
+const (
+	unknownRank rankEnum = iota
+	cadetRank
+	lieutenantRank
+	captainRank
+	admiralRank
+)
+
+// must match order for rankEnum
+var legalRanks = [...]string{
+	"unknown",
+	"cadet",
+	"lieutenant",
+	"captain",
+	"admiral",
+}
+
+// must match order for rankEnum
+func (re rankEnum) string() string {
+	return [...]string{"unknown", "cadet", "lieutenant", "captain", "admiral"}[re]
+}
+
+func findRank(arg string) rankEnum {
+	for ndx := 0; ndx < len(legalRanks); ndx++ {
+		if legalRanks[ndx] == arg {
+			return rankEnum(ndx)
+		}
+	}
+
+	return rankEnum(unknownRank)
+}
+
+// teamEnum
+type teamEnum int
+
+// must match order for legalTeams
+const (
+	unknownTeam teamEnum = iota
+	neutralTeam
+	blueTeam
+	redTeam
+	acheronTeam
+)
+
+// must match order for teamEnum
+var legalTeams = [...]string{
+	"unknown",
+	"neutral",
+	"blue",
+	"red",
+	"acheron",
+}
+
+// must match order for teamEnum
+func (te teamEnum) string() string {
+	return [...]string{"unknown", "neutral", "blue", "red", "acheron"}[te]
+}
+
+func findTeam(arg string) teamEnum {
+	for ndx := 0; ndx < len(legalTeams); ndx++ {
+		if legalTeams[ndx] == arg {
+			return teamEnum(ndx)
+		}
+	}
+
+	return teamEnum(unknownTeam)
+}
+
+type playerType struct {
+	email string
+	name  string
+	rank  rankEnum
+	score int
+	team  teamEnum
+	uuid  string
+}
+
+const maxTeamPlayers = 5
+const maxPlayers = maxTeamPlayers * 2
+
+// playerArrayType contains all active players
+type playerArrayType [maxPlayers]*playerType
+
+// newPlayer convenience function to populate struct
+func newPlayer(name, id string, rank string, team string) (*playerType, error) {
+	if len(id) < 1 {
+		return nil, errors.New("bad player id")
+	}
+
+	if len(name) < 1 {
+		return nil, errors.New("bad player name")
+	}
+
+	result := playerType{name: name, uuid: id}
+	playerRank := findRank(rank)
+	if playerRank == unknownRank {
+		return nil, errors.New("unknown rank")
+	}
+
+	result.rank = playerRank
+
+	playerTeam := findTeam(team)
+	if playerTeam == unknownTeam {
+		return nil, errors.New("unknown team")
+	}
+
+	result.team = playerTeam
+
+	return &result, nil
+}
+
+const testPlayerID1 = "testId1"
+const testPlayerName1 = "testName1"
+
+// testPlayer1 returns test player1
+func testPlayer1() *playerType {
+	np1, _ := newPlayer(testPlayerName1, testPlayerID1, "cadet", "blue")
+	return np1
+}
+
+const testPlayerID2 = "testId2"
+const testPlayerName2 = "testName2"
+
+// testPlayer2 returns test player2
+func testPlayer2() *playerType {
+	np2, _ := newPlayer(testPlayerName2, testPlayerID2, "admiral", "red")
+	return np2
+}
+
+// playerAdd adds player to array
+func playerAdd(pt *playerType, pat *playerArrayType) int {
+	log.Printf("playerAdd:%s %s", pt.name, pt.uuid)
+
+	for ndx := 0; ndx < maxPlayers; ndx++ {
+		if pat[ndx] == nil {
+			pat[ndx] = pt
+			return ndx
+		}
+	}
+
+	return -1
+}
+
+// playerCensus returns population of red/blue players
+func playerCensus(pat playerArrayType) (int, int) {
+	bluePopulation := 0
+	redPopulation := 0
+
+	for ndx := 0; ndx < maxPlayers; ndx++ {
+		if pat[ndx] != nil {
+			switch pat[ndx].team {
+			case blueTeam:
+				bluePopulation++
+			case redTeam:
+				redPopulation++
+			}
+		}
+	}
+
+	return bluePopulation, redPopulation
+}
+
+// playerDelete removes player from array
+func playerDelete(target string, pat *playerArrayType) int {
+	log.Printf("playerDelete:%s", target)
+
+	for ndx := 0; ndx < maxPlayers; ndx++ {
+		if pat[ndx] != nil {
+			if strings.Compare(pat[ndx].uuid, target) == 0 {
+				pat[ndx] = nil
+				return ndx
+			}
+		}
+	}
+
+	return -1
+}
+
+// playerDump diagnostic
+func playerDump(pat playerArrayType) {
+	log.Println("=-=-=-= playerDump =-=-=-=")
+
+	for ndx := 0; ndx < maxPlayers; ndx++ {
+		if pat[ndx] == nil {
+			log.Printf("%d nil", ndx)
+		} else {
+			rank := pat[ndx].rank.string()
+			team := pat[ndx].team.string()
+
+			log.Printf("%d %s %s %s %s", ndx, pat[ndx].name, rank, team, pat[ndx].uuid)
+		}
+	}
+
+	log.Println("=-=-=-= playerDump =-=-=-=")
+}
+
+// playerFind returns array index for player by uuid
+func playerFind(target string, pat playerArrayType) int {
+	for ndx := 0; ndx < maxPlayers; ndx++ {
+		if pat[ndx] != nil {
+			if strings.Compare(pat[ndx].uuid, target) == 0 {
+				return ndx
+			}
+		}
+	}
+
+	return -1
+}
+
+/*
+// commandPlayerCreate services command
+func commandPlayerCreate(ct commandType, gt *gameType) error {
+	duplicate := playerFind(ct.player, gt.players)
+	if duplicate >= 0 {
+		return errors.New("duplicate player id")
+	}
+
+	// TODO test for max players per side
+
+	np, err := newPlayer(ct.args[1], ct.player, ct.args[2], ct.args[3])
+	if err != nil {
+		return errors.New("newPlayer creation failure")
+	}
+
+	if np == nil {
+		return errors.New("newPlayer returns nil")
+	}
+
+	playerAdd(np, &gt.players)
+
+	return nil
+}
+*/
+
+/*
+// commandPlayerDelete services command
+func commandPlayerDelete(ct commandType, gt *gameType) {
+	// TODO delete ship
+	playerDelete(ct.player, &gt.players)
+}
+*/
+
+/*
+	//store in redis
+	err = rdb.Set(ctx, ipaddress, &ipStackResponseSuccess, time.Hour*24).Err()
+	if err != nil {
+		log.Error(err)
+	}
+	return &ipStackResponseSuccess, nil
+*/
---
+++ manager/player_test.go
@@ -0,0 +1,211 @@
+// Copyright 2021 Guy Cole. All rights reserved.
+// Use of this source code is governed by a GPL-3 license that can be found
+// in the LICENSE file.
+package main
+
+import (
+	"testing"
+)
+
+func TestPlayerRank(t *testing.T) {
+	tests := []struct {
+		candidate string
+		answer    rankEnum
+	}{
+		{"cadet", cadetRank},
+		{"admiral", admiralRank},
+		{"bogus", unknownRank},
+	}
+	for _, ndx := range tests {
+		result := findRank(ndx.candidate)
+		if result != ndx.answer {
+			t.Errorf("findRank(%s) failure", ndx.candidate)
+		}
+	}
+}
+
+func TestPlayerTeam(t *testing.T) {
+	tests := []struct {
+		candidate string
+		answer    teamEnum
+	}{
+		{"neutral", neutralTeam},
+		{"red", redTeam},
+		{"bogus", unknownTeam},
+	}
+	for _, ndx := range tests {
+		result := findTeam(ndx.candidate)
+		if result != ndx.answer {
+			t.Errorf("findTeam(%s) failure", ndx.candidate)
+		}
+	}
+}
+
+func TestNewOkPlayer(t *testing.T) {
+	result, err := newPlayer(testPlayerName1, testPlayerID1, "cadet", "blue")
+	if err != nil {
+		t.Errorf("newPlayer error:%s", err)
+	}
+
+	if result != nil {
+		if result.name != testPlayerName1 {
+			t.Error("newPlayer name failure")
+		}
+
+		if result.rank != cadetRank {
+			t.Error("newPlayer rank failure")
+		}
+
+		if result.team != blueTeam {
+			t.Error("newPlayer team failure")
+		}
+
+		if result.uuid != testPlayerID1 {
+			t.Error("newPlayer id failure")
+		}
+	} else {
+		t.Error("newPlayer returns nil")
+	}
+}
+
+func TestNewBadPlayer01(t *testing.T) {
+	result, err := newPlayer("", testPlayerID2, "cadet", "blue")
+	if err == nil {
+		t.Error("newPlayer error:expecting bad player")
+	}
+
+	if result != nil {
+		t.Error("newPlayer error expecting nil")
+	}
+}
+
+func TestNewBadPlayer02(t *testing.T) {
+	result, err := newPlayer(testPlayerName2, "", "cadet", "blue")
+	if err == nil {
+		t.Error("newPlayer error:expecting bad id")
+	}
+
+	if result != nil {
+		t.Error("newPlayer error expecting nil")
+	}
+}
+
+func TestNewBadPlayer03(t *testing.T) {
+	result, err := newPlayer(testPlayerName2, testPlayerID2, "", "blue")
+	if err == nil {
+		t.Error("newPlayer error:expecting bad rank")
+	}
+
+	if result != nil {
+		t.Error("newPlayer error expecting nil")
+	}
+}
+
+func TestNewBadPlayer04(t *testing.T) {
+	result, err := newPlayer(testPlayerName2, testPlayerID2, "cadet", "")
+	if err == nil {
+		t.Error("newPlayer error:expecting bad team")
+	}
+
+	if result != nil {
+		t.Error("newPlayer error expecting nil")
+	}
+}
+
+func TestPlayerArray(t *testing.T) {
+	var pat playerArrayType
+
+	bluePopulation, redPopulation := playerCensus(pat)
+	if bluePopulation != 0 && redPopulation != 0 {
+		t.Errorf("playerCensus error:%d %d", bluePopulation, redPopulation)
+	}
+
+	np1 := testPlayer1()
+	if np1 == nil {
+		t.Error("testPlayer1 returns nil")
+	}
+
+	np2 := testPlayer2()
+	if np2 == nil {
+		t.Error("testPlayer2 returns nil")
+	}
+
+	// add player to player array, should be first array element
+	ndx := playerAdd(np1, &pat)
+	if ndx != 0 {
+		t.Errorf("playerAdd returns wrong index %d", ndx)
+	}
+
+	// add player to player array, should be second array element
+	ndx = playerAdd(np2, &pat)
+	if ndx != 1 {
+		t.Errorf("playerAdd returns wrong index %d", ndx)
+	}
+
+	bluePopulation, redPopulation = playerCensus(pat)
+	if bluePopulation != 1 && redPopulation != 1 {
+		t.Errorf("playerCensus error:%d %d", bluePopulation, redPopulation)
+	}
+
+	// playerDump(pat)
+
+	ndx = playerFind(testPlayerID1, pat)
+	if ndx != 0 {
+		t.Errorf("playerFind returns wrong index %d", ndx)
+	}
+
+	ndx = playerFind(testPlayerID2, pat)
+	if ndx != 1 {
+		t.Errorf("playerFind returns wrong index %d", ndx)
+	}
+
+	ndx = playerFind("bogus", pat)
+	if ndx >= 0 {
+		t.Errorf("playerFind returns wrong index %d", ndx)
+	}
+
+	ndx = playerDelete(testPlayerID1, &pat)
+	if ndx != 0 {
+		t.Errorf("playerDelete returns wrong index %d", ndx)
+	}
+
+	bluePopulation, redPopulation = playerCensus(pat)
+	if bluePopulation != 0 && redPopulation != 1 {
+		t.Errorf("playerCensus error:%d %d", bluePopulation, redPopulation)
+	}
+}
+
+func TestCreateDeletePlayer(t *testing.T) {
+	ct := commandType{player: testPlayerID2, request: "requestId"}
+	ct.args = []string{"playerCreate", testPlayerName1, "captain", "blue"}
+	ct.command = playerCreateCommand
+
+	gt := gameType{uuid: "gameId"}
+
+	err := commandPlayerCreate(ct, &gt)
+	if err != nil {
+		t.Errorf("commandCreatePlayer error:%s", err)
+	}
+
+	//playerDump(gt.players)
+
+	bluePopulation, redPopulation := playerCensus(gt.players)
+	if bluePopulation != 1 && redPopulation != 0 {
+		t.Errorf("playerCensus error:%d %d", bluePopulation, redPopulation)
+	}
+
+	err = commandPlayerCreate(ct, &gt)
+	if err == nil {
+		t.Errorf("commandCreatePlayer should have duplicate error")
+	}
+
+	// playerDump(gt.players)
+
+	ct = commandType{player: testPlayerID2, request: "requestId"}
+	ct.args = []string{"playerDelete"}
+	ct.command = playerDeleteCommand
+
+	commandPlayerDelete(ct, &gt)
+
+	// playerDump(gt.players)
+}
