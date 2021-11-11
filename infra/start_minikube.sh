9a67cf7c9cca1ac07979ba9e219f4299ae03bf76
Guy Cole <guycole@gmail.com>
2021-11-10 21:19:17 -0800

    broken ingress deployment

--- infra/start_minikube.sh
+++ infra/start_minikube.sh
@@ -6,4 +6,5 @@
 # Author: G.S. Cole (guycole at gmail dot com)
 #
 minikube start --memory=6g --nodes=1 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0 --vm=true --kubernetes-version=v1.20.0
+minikube addons enable ingress
 #
